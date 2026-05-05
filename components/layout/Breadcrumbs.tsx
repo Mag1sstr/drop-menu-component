c207bcc5db134ebf9094646bd0496587f0f69d39
Daniil K <danilkarachev234@mail.ru>
2026-05-06 02:48:06 +0500

    update

---
+++ components/layout/Breadcrumbs.tsx
@@ -0,0 +1,10 @@
+function Breadcrumbs() {
+  return (
+    <div className="bg-white">
+      <div className="container">
+        <div className="py-4 flex gap-4"></div>
+      </div>
+    </div>
+  );
+}
+export default Breadcrumbs;
