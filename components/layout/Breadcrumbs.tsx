b9aff5e2f8d80fc1643e71b8cc58aba81d7dd2e1
Daniil K <danilkarachev234@mail.ru>
2026-05-10 04:16:03 +0500

    update

--- components/layout/Breadcrumbs.tsx
+++ components/layout/Breadcrumbs.tsx
@@ -11,40 +11,46 @@ function Breadcrumbs() {
     catalog: "каталог",
   };
   return (
-    <div className="bg-white">
-      <div className="container">
-        <div className="py-4 flex gap-4">
-          {fullPath.map((el, i) => (
-            <div
-              onClick={() => {
-                const path =
-                  "/" +
-                  fullPath
-                    .slice(0, i + 1)
-                    .filter((v) => v !== "Главная")
-                    .join("/");
+    fullPath.length - 1 > 0 && (
+      <div className="bg-white">
+        <div className="container">
+          <div className="py-4 flex gap-4">
+            {fullPath.map((el, i) => (
+              <div
+                onClick={() => {
+                  const path =
+                    "/" +
+                    fullPath
+                      .slice(0, i + 1)
+                      .filter((v) => v !== "Главная")
+                      .join("/");
 
-                router.push(path);
-              }}
-              className={`flex items-center gap-4  font-medium text-[12px] uppercase leading-none ${el === fullPath.at(-1) ? "text-[#1D1D1D]" : "text-[#C53720] cursor-pointer"}`}
-            >
-              {breadcrumbsNames[el] || el}
-              {el !== fullPath.at(-1) && (
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M8 4L12 10L8 16" stroke="#C53720" strokeWidth="4" />
-                </svg>
-              )}
-            </div>
-          ))}
+                  router.push(path);
+                }}
+                className={`flex items-center gap-4  font-medium text-[12px] uppercase leading-none ${el === fullPath.at(-1) ? "text-[#1D1D1D]" : "text-[#C53720] cursor-pointer"}`}
+              >
+                {breadcrumbsNames[el] || el}
+                {el !== fullPath.at(-1) && (
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8 4L12 10L8 16"
+                      stroke="#C53720"
+                      strokeWidth="4"
+                    />
+                  </svg>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 export default Breadcrumbs;
