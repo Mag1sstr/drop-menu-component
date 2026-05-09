00525b8eb785d373470ff4a9b4c6dbdc073bbf60
Daniil K <danilkarachev234@mail.ru>
2026-05-10 03:16:47 +0500

    update

--- components/providers/Provider.tsx
+++ components/providers/Provider.tsx
@@ -3,10 +3,12 @@
 import { store } from "@/store/store";
 import { Provider } from "react-redux";
 import AuthContextProvider from "./AuthContext";
+import ScrollBtn from "../ui/ScrollBtn";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
+      <ScrollBtn />
       <AuthContextProvider>{children}</AuthContextProvider>
     </Provider>
   );
---
+++ components/ui/ScrollBtn.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+
+function ScrollBtn() {
+  const [isTop, setIsTop] = useState(false);
+  const [isBottom, setIsBottom] = useState(false);
+
+  const handleScrollToTop = () =>
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  const handleScrollToBottom = () =>
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+
+  console.log(document.body.scrollHeight);
+
+  useEffect(() => {
+    const hadnleScroll = () => {
+      setIsTop(window.scrollY <= 0);
+      setIsBottom(window.scrollY >= document.body.scrollHeight);
+    };
+    document.addEventListener("scroll", hadnleScroll);
+
+    return () => document.removeEventListener("scroll", hadnleScroll);
+  }, []);
+
+  return (
+    <div className="flex flex-col fixed bottom-10 right-10.5 gap-3 z-50">
+      <button
+        onClick={handleScrollToTop}
+        className={`group cursor-pointer ${isTop && "opacity-50"}`}
+      >
+        <svg
+          width="52"
+          height="52"
+          viewBox="0 0 52 52"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="group-hover:bg-[#C53720] transition-all group-hover:[&>path]:stroke-white"
+        >
+          <rect
+            x="2"
+            y="2"
+            width="48"
+            height="48"
+            transform="matrix(-4.37114e-08 1 1 4.37114e-08 2.18557e-06 8.74228e-08)"
+            stroke="#C53720"
+            strokeWidth="4"
+          />
+          <path d="M12 32L26 20L40 32" stroke="#C53720" stroke-width="4" />
+        </svg>
+      </button>
+      <button
+        onClick={handleScrollToBottom}
+        className={`group rotate-180 ${isBottom && "opacity-50"}`}
+      >
+        <svg
+          width="52"
+          height="52"
+          viewBox="0 0 52 52"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="group-hover:bg-[#C53720] transition-all group-hover:[&>path]:stroke-white"
+        >
+          <rect
+            x="2"
+            y="2"
+            width="48"
+            height="48"
+            transform="matrix(-4.37114e-08 1 1 4.37114e-08 2.18557e-06 8.74228e-08)"
+            stroke="#C53720"
+            strokeWidth="4"
+          />
+          <path d="M12 32L26 20L40 32" stroke="#C53720" stroke-width="4" />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default ScrollBtn;
