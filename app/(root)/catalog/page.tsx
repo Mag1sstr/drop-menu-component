00a9cdf4aea8865c52dc93226556f5e208bc9ec8
Daniil K <danilkarachev234@mail.ru>
2026-05-11 05:21:37 +0500

    update

--- app/(root)/catalog/page.tsx
+++ app/(root)/catalog/page.tsx
@@ -1,9 +1,11 @@
 import Breadcrumbs from "@/components/layout/Breadcrumbs";
+import Catalog from "@/components/pages/Catalog";
 
 function CatalogPage() {
   return (
     <>
       <Breadcrumbs />
+      <Catalog />
     </>
   );
 }
