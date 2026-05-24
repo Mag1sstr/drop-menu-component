eef6a26f6203a828e4f2c1caa98200bac55194a8
Daniil K <danilkarachev234@mail.ru>
2026-05-25 04:59:06 +0500

    update

--- app/(root)/catalog/page.tsx
+++ app/(root)/catalog/page.tsx
@@ -1,45 +1,8 @@
+import Catalog from "@/components/pages/Catalog";
 import ProductCard from "@/components/ui/ProductCard";
 
 function CatalogPage() {
-  return (
-    <section>
-      <div className="flex justify-between mb-5">
-        <ul className="flex [&>li]:flex [&>li]:items-center [&>li]:gap-1 text-[14px] font-medium uppercase">
-          <li className="uppercase mr-5 ">Сортировать</li>
-          <li className="text-[#1D1D1D]/50 mr-2">
-            Цене{" "}
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M4 8L10 12L16 8" stroke="#C53720" strokeWidth="4" />
-            </svg>
-          </li>
-          <li className="text-[#1D1D1D]/50">
-            ПО НАЛИЧИЮ{" "}
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M4 8L10 12L16 8" stroke="#C53720" strokeWidth="4" />
-            </svg>
-          </li>
-        </ul>
-      </div>
-      <div className="grid grid-cols-3 gap-6">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
-    </section>
-  );
+  return <Catalog />;
 }
 
 export default CatalogPage;
--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -8,22 +8,26 @@ interface IProps {
 
 function ProductCard({ title, description, price }: IProps) {
   return (
-    <div className="relative px-5 py-6.75 border-4 border-[#3CC051] font-medium">
+    <div className="relative px-5 py-6.75 border-4 border-[#3CC051] font-medium flex flex-col bg-white group">
       <div className="uppercase text-[10px] text-white w-22 text-center py-2 absolute bg-[#3CC051] right-0 top-0">
         В НАЛИЧИИ
       </div>
-      <div className="flex-1 mt-4.25 h-37 mb-4.5">
-        <img className="w-full h-full object-cover" src="dwd" alt="" />
+      <div className="mt-4.25 h-37 mb-4.5">
+        <img className="w-full h-full object-contain" src="/pr.png" alt="" />
       </div>
-      <h2 className="text-[20px] uppercase font-medium mb-5">
-        {title ?? "MAGNUM 60Ah"}
-      </h2>
-      <p className="font-bold text-[28px] mb-3">{price ?? 2100} руб.</p>
-      <p className="text-[#A5A5A5] text-[12px] mb-5">
-        {description ??
-          "Цена действительна при сдаче старого аккумулятора аналогичной емкости в лом"}
-      </p>
-      <Button className="text-(--prime)! w-full">ПОДРОБНЕЕ</Button>
+      <div className="flex-1 flex flex-col">
+        <div>
+          <h2 className="text-[20px] uppercase font-medium mb-5 transition-all group-hover:text-(--prime)">
+            {title ?? "MAGNUM 60Ah"}
+          </h2>
+          <p className="font-bold text-[28px] mb-3">{price ?? 2100} руб.</p>
+        </div>
+        <p className="text-[#A5A5A5] text-[12px] mb-5">
+          {description ??
+            "Цена действительна при сдаче старого аккумулятора аналогичной емкости в лом"}
+        </p>
+        <Button className="text-(--prime)! w-full mt-auto">ПОДРОБНЕЕ</Button>
+      </div>
     </div>
   );
 }
