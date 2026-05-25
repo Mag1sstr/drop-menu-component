95099292dbab87dd04576c711483d5c93f4be6f8
Daniil K <danilkarachev234@mail.ru>
2026-05-25 05:16:47 +0500

    update

--- store/zustand/useCart.ts
+++ store/zustand/useCart.ts
@@ -1,9 +1,35 @@
+import { IProduct } from "@/app/types";
 import { create } from "zustand";
-
+import { persist } from "zustand/middleware";
+interface ICart extends IProduct {
+  count: number;
+}
 interface ICartStore {
-  cart: [];
+  cart: ICart[];
 }
 
-export const useCart = create((set) => ({
-  cart: [],
-}));
+export const useCart = create<ICartStore>()(
+  persist(
+    (set) => ({
+      cart: [],
+      addCartItem: (item: ICart) =>
+        set((state) => ({ cart: [...state.cart, item] })),
+      deleteCartItem: (id: number) =>
+        set((state) => ({ cart: state.cart.filter((el) => el.id !== id) })),
+      increaseCartItem: (id: number) =>
+        set((state) => ({
+          cart: state.cart.map((el) =>
+            el.id === id ? { ...el, count: el.count + 1 } : el,
+          ),
+        })),
+      decreaseCartItem: (id: number) =>
+        set((state) => ({
+          cart: state.cart.map((el) =>
+            el.id === id && el.count > 1 ? { ...el, count: el.count - 1 } : el,
+          ),
+        })),
+      clearCart: () => set({ cart: [] }),
+    }),
+    { name: "cart" },
+  ),
+);
