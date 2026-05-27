7bba1c5586f6b736e59afacb96c4f003154b2218
Daniil K <danilkarachev234@mail.ru>
2026-05-27 05:26:36 +0500

    update

--- store/zustand/useFilters.ts
+++ store/zustand/useFilters.ts
@@ -1,3 +1,19 @@
 import { create } from "zustand";
-
-export const useFilters = create((set) => ({}));
+interface IFilters {
+  rangePrice: {
+    price_min: string;
+    price_max: string;
+  };
+  setMaxPrice: (p: string) => void;
+  setMinPrice: (p: string) => void;
+}
+export const useFilters = create<IFilters>((set) => ({
+  rangePrice: {
+    price_min: "",
+    price_max: "",
+  },
+  setMaxPrice: (price: string) =>
+    set((state) => ({ rangePrice: { ...state.rangePrice, price_max: price } })),
+  setMinPrice: (price: string) =>
+    set((state) => ({ rangePrice: { ...state.rangePrice, price_min: price } })),
+}));
