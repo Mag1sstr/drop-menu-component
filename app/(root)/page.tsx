6b48517230e1879f88ef736def6b3762f0784cf5
Daniil K <danilkarachev234@mail.ru>
2026-06-09 04:28:13 +0500

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import Breadcrumbs from "@/components/layout/Breadcrumbs";
+import PartnersSlider from "@/components/layout/PartnersSlider";
 
 export default function Home() {
   return (
     <main>
       <Breadcrumbs />
+      <PartnersSlider />
     </main>
   );
 }
---
+++ components/layout/PartnersSlider.tsx
@@ -0,0 +1,27 @@
+function PartnersSlider() {
+  return (
+    <section className="bg-white py-13.75">
+      <div className="container overflow-hidden">
+        <div className="flex w-max scrollPartners">
+          <div className="flex gap-11 items-center shrink-0">
+            <img src="/partners/1.png" alt="icon" />
+            <img src="/partners/2.png" alt="icon" />
+            <img src="/partners/3.png" alt="icon" />
+            <img src="/partners/4.png" alt="icon" />
+            <img src="/partners/5.png" alt="icon" />
+          </div>
+
+          <div className="flex gap-11 items-center shrink-0">
+            <img src="/partners/1.png" alt="icon" />
+            <img src="/partners/2.png" alt="icon" />
+            <img src="/partners/3.png" alt="icon" />
+            <img src="/partners/4.png" alt="icon" />
+            <img src="/partners/5.png" alt="icon" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PartnersSlider;
