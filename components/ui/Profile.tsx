8d1d30593b75786f434f05d2ca74c821692f42a5
Daniil K <danilkarachev234@mail.ru>
2026-07-18 02:48:23 +0500

    update

--- components/ui/Profile.tsx
+++ components/ui/Profile.tsx
@@ -1,24 +1,35 @@
 "use client";
 import { useState } from "react";
 import AuthModal from "../modals/AuthModal";
-import { useAppDispatch, useAppSelector } from "@/store/store";
-import { logout } from "@/store/authSlice";
-import { useGetUserMutation } from "@/store/frostApi";
 import { useAuth } from "@/contexts/AuthContext";
 
 function Profile() {
-  const { user, setToken, setUser, isUserLoading } = useAuth();
+  const { user, setToken, setUser, isUserLoading, logout } = useAuth();
   const [openModal, setOpenModal] = useState(false);
 
   return (
     <>
       {user ? (
-        <button
-          className="uppercase border-4 border-(--prime) px-5 py-3 text-[12px] text-white self-center font-bold mr-5.75 whitespace-nowrap max-w-30 overflow-hidden text-ellipsis cursor-pointer transition-all hover:bg-white hover:text-black"
-          title={user.firstName + " " + user.lastName}
-        >
-          {`${user.firstName} ${user.lastName}`}
-        </button>
+        <div className="relative self-center max-w-30">
+          <button
+            className="uppercase border-4 border-(--prime) px-5 py-3 text-[12px] text-white self-center font-bold whitespace-nowrap w-full overflow-hidden text-ellipsis cursor-pointer transition-all hover:bg-white hover:text-black"
+            title={user.firstName + " " + user.lastName}
+          >
+            {`${user.firstName} ${user.lastName}`}
+          </button>
+          <div className="absolute z-50  top-full mt-2.5 right-0 w-50 py-2.5 bg-zinc-700 text-white">
+            <div
+              onClick={() => {
+                logout();
+              }}
+              className="px-3 group cursor-pointer"
+            >
+              <div className="border-b border-white py-3 group-hover:border-(--prime) group-hover:text-(--prime)">
+                Выйти
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
         <button
           onClick={() => {
